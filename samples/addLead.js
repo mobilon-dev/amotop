a93c5dad82706be462cfca173a819843657a44b5
sergey <110@mobilon.ru>
2024-09-09 08:43:44 +0700

    fix

--- samples/addLead.js
+++ samples/addLead.js
@@ -6,16 +6,27 @@ const {domain, accessToken, debug} = require('./_config');
 const amoApiClient = new AmoApiClient(domain, accessToken, {debug});
 
 const start = async () => {
-  try {    
-    
-    const lead = {
-      ...amoApiClient.getBaseLeadPayload('Продать слона', 1000),
-      ...amoApiClient.getPipelineLeadPayload(7915998, 65052502),
+  try {
+    // просто передаем объект сделки
+    const lead1 = {
+      name: 'Продать слона',
+      price: 1000,
     };
-    console.log('lead payload', lead);
+    console.log('lead payload', lead1);
 
-    const leadResponse = await amoApiClient.addLead(lead);
-    console.log('leadResponse', JSON.stringify(leadResponse, null, 2));
+    const leadResponse1 = await amoApiClient.addLead(lead1);
+    console.log('leadResponse', JSON.stringify(leadResponse1, null, 2));
+
+
+    // используем хелперы для payload
+    const lead2 = {
+      ...amoApiClient.getBaseLeadPayload('Продать слона', 1000),
+      ...amoApiClient.getPipelineLeadPayload(7183562, 60002878),
+    };
+    console.log('lead payload', lead2);
+
+    const leadResponse2 = await amoApiClient.addLead(lead2);
+    console.log('leadResponse', JSON.stringify(leadResponse2, null, 2));
 
   } catch (err) {
     const errMessage = err.response?.data ? JSON.stringify(err.response.data, null, 2) : err;
@@ -24,3 +35,11 @@ const start = async () => {
 }   
 
 (start)();
+
+
+/*
+
+[AmoApiClient][Request] POST https://mobilontestdev.amocrm.ru/api/v4/leads [{"name":"Продать слона","price":1000}]
+[AmoApiClient][Response] POST https://mobilontestdev.amocrm.ru/api/v4/leads 200:OK {"_links":{"self":{"href":"https://mobilontestdev.amocrm.ru/api/v4/leads"}},"_embedded":{"leads":[{"id":40885329,"request_id":"0","_links":{"self":{"href":"https://mobilontestdev.amocrm.ru/api/v4/leads/40885329"}}}]}}
+
+*/
